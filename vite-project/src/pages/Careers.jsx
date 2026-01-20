7d65f878cbf334228ccfef9743300e7602d5453e
JawaSalman <jawasalman376@gmail.com>
2026-01-20 17:40:17 +0000

    added css

--- vite-project/src/pages/Careers.jsx
+++ vite-project/src/pages/Careers.jsx
@@ -1,10 +1,14 @@
 import OurValues from "../components/Careers components/OurValues";
+import FAQs from "../components/Shared components/FAQs/FAQs";
+import FinancialCTA from "../components/Shared components/FinancialCTA/FinancialCTA";
 
 const Careers = () => {
     return (
         <>
             <section>
-                <OurValues/>
+                <OurValues />
+                <FAQs />
+                <FinancialCTA />
             </section>
         </>
     )
