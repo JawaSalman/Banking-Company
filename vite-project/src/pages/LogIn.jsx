cacdf9bcc0b16db046479373a3a8a3aed5494cb9
JawaSalman <jawasalman376@gmail.com>
2026-01-24 16:04:18 +0000

    cleaned the code

--- vite-project/src/pages/LogIn.jsx
+++ vite-project/src/pages/LogIn.jsx
@@ -1,6 +1,5 @@
 import OurTestimonials from "../components/Shared components/OurTestimonials/OurTestimonials"
-import Login from "../components/Login/Login"
-
+import Login from "../components/Login components/Login/Login"
 const LogIn = () => {
     return (
         <div>
