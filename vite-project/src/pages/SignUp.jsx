c4327328bb92d28ad2ea60f38723b38b2d29e035
JawaSalman <jawasalman376@gmail.com>
2026-01-25 11:05:33 +0000

    added maysa section after new css

--- vite-project/src/pages/SignUp.jsx
+++ vite-project/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import OurTestimonials from "../components/Shared components/OurTestimonials/OurTestimonials"
+import Sign from "../components/Shared components/Sign/Sign"
 
 const SignUp = () => {
     return (
         <div>
+            <Sign />
             <OurTestimonials />
-
         </div>
     )
 }
