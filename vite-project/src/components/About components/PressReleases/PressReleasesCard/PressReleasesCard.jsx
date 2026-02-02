64d4981831c503b49411c557cbb63a551e48e70a
Hayyan Al-Sayyed <hayyansayyed2@gmail.com>
2026-02-02 20:30:20 +0300

    Add Press Releases section, animation, and general improvement

--- vite-project/src/components/About components/PressReleases/PressReleasesCard/PressReleasesCard.jsx
+++ vite-project/src/components/About components/PressReleases/PressReleasesCard/PressReleasesCard.jsx
@@ -1,28 +1,52 @@
 import "./PressReleasesCard.css";
+// Framer Motion
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { cardHover, containerVariants, fadeIn, staggerContainer } from "../../../Animation/Animation";
 
 const PressReleasesCard = ({ card }) => {
   return (
 
-    <div className="hs-pr-card border">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      whileHover="hover"
+      viewport={{ once: true, amount: 0.1 }}
+      {...cardHover}
 
-      <div className="hs-pr-card-image">
+      className="hs-pr-card border">
+
+      <motion.div
+        variants={fadeIn(0.2)}
+        className="hs-pr-card-image">
         <img src={card.image} alt={card.title} />
-      </div>
+      </motion.div>
 
       <div className="hs-pr-card-content">
 
-        <h3 className="hs-pr-card-title">{card.title}</h3>
+        <motion.h3
+          variants={fadeIn("down", 0.3)}
+          className="hs-pr-card-title">{card.title}</motion.h3>
 
-        <div className="hs-pr-card-tags">
-          <p className="hs-pr-tag border">Location: {card.location}</p>
-          <p className="hs-pr-tag border">Date: {card.date}</p>
-        </div>
+        <motion.div
+          variants={staggerContainer}
+          className="hs-pr-card-tags">
+          <motion.p
+            variants={fadeIn("up", 0.4)}
+            className="hs-pr-tag border">Location: {card.location}</motion.p>
+          <motion.p
+            variants={fadeIn("up", 0.5)}
+            className="hs-pr-tag border">Date: {card.date}</motion.p>
+        </motion.div>
 
-        <p className="hs-pr-card-description">{card.description}</p>
+        <motion.p
+          variants={fadeIn("up", 0.6)}
+          className="hs-pr-card-description">{card.description}</motion.p>
 
       </div>
 
-    </div>
+    </motion.div>
 
   );
 };
--- vite-project/src/components/About components/PressReleases/PressReleasesHeader/PressReleasesHeader.jsx
+++ vite-project/src/components/About components/PressReleases/PressReleasesHeader/PressReleasesHeader.jsx
@@ -1,14 +1,33 @@
 import './PressReleasesHeader.css';
 
+// Framer Motion
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+import { fadeIn, containerVariants } from "../../../Animation/Animation";
+
 const PressReleasesHeader = ({ title, description }) => {
   return (
-    <div className="hs-pr-header">
-      <h1 className="hs-pr-header-title">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, margin: "0px" }}
+      className="hs-pr-header"
+    >
+      <motion.h1
+        variants={fadeIn("down", 0.1, 0.8)}
+        className="hs-pr-header-title"
+      >
         <span>{title}</span>
-      </h1>
-      <p className="hs-pr-header-description">{description}</p>
+      </motion.h1>
 
-    </div>
+      <motion.p
+        variants={fadeIn("up", 0.3, 0.8)}
+        className="hs-pr-header-description"
+      >
+        {description}
+      </motion.p>
+    </motion.div>
   );
 };
 
